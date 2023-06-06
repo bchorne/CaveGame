322ad5e9f8cbf9db326c0e244809d619602eec16
bchorne <ben.chris.horne@gmail.com>
2023-06-06 16:53:50 +1200

    Wrote code for changing speed on entering the appropriate trigger, and reducing speed to zero unless the mouse has moved horizontally in the last frame

--- Assets/Player/Movement.cs
+++ Assets/Player/Movement.cs
@@ -8,7 +8,9 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     [SerializeField] CharacterController controller;
+    [SerializeField] MouseLook mouse;
     [SerializeField] float speed = 11f;
+    private float storedSpeed = 0f;
     Vector2 horizontalInput;
 
     [SerializeField] float gravity = -30;
@@ -18,6 +20,9 @@ public class Movement : MonoBehaviour
     private float groundedTimer;
     public bool jump;
 
+    private bool squeezing;
+    private float sqSpeed; //Speed before entering the squeeze zone
+
     public void ReceiveInput (Vector2 _horizontalInput)
     {
         horizontalInput = _horizontalInput;
@@ -25,6 +30,18 @@ public class Movement : MonoBehaviour
 
     void Update()
     {
+        //Check Squeezing
+
+        if(squeezing && !mouse.wiggle && speed != 0)
+        {
+            storedSpeed = speed;
+            speed = 0; //Halt player movement unless mouse has moved horizontally since last frame while in a Squeeze space
+        }
+        else if (mouse.wiggle && squeezing)
+        {
+            speed = storedSpeed;
+        }
+
         //Move player based on horizontal input
         Vector3 horizontalVelocity = (transform.right * horizontalInput.x + transform.forward * horizontalInput.y) * speed;
         controller.Move(horizontalVelocity * Time.deltaTime);
@@ -60,5 +77,26 @@ public class Movement : MonoBehaviour
     public void OnJumpPressed()
     {
         jump = true;
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.CompareTag("Squeeze"))
+        {
+            float mult = coll.GetComponent<SqueezeCollider>().speedMulti;
+            sqSpeed = speed;
+            squeezing = true;
+
+            speed = speed * mult;
+        }
+    }
+
+    void OnTriggerExit(Collider coll)
+    {
+        if (coll.CompareTag("Squeeze"))
+        {
+            speed = sqSpeed;
+            squeezing = false;
+        }
     }
 }
